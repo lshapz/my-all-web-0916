e12fd13a62dbfb86e70b0c53e672227f04a29c89
Katie Hoffman <ktahoffman@gmail.com>
2015-04-02 16:10:30 -0400

    add unrefactored solution

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -2,6 +2,16 @@
 
 module Enumerable
   def my_all?
-    
+    each do |value|
+      begin
+        val = yield(value)
+      rescue
+        block = lambda { |i| i }
+        val = block.call(value)
+      end
+      true_or_false = !!val
+      return false if true_or_false == false
+    end
+    true
   end
 end
