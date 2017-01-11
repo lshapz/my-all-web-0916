a11eb5074ae6e176bdfad5ac7461043fc8391b64
lshapz <laurashapiro01@gmail.com>
2017-01-11 17:11:17 -0500

    Done.

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,1 +1,19 @@
 # add the method #my_all? to the Enumerable module
+module Enumerable
+  def my_all?
+    
+   self.each do |item| 
+    begin 
+      val = yield item
+    rescue
+     block = lambda {|i| i}
+     val = block.call(item)
+    end
+    truthiness = !!val
+    return false if truthiness == false
+
+  end
+  true
+  end
+
+end 
